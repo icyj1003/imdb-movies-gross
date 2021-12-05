dfe33bffc28224203a46b0ad0de5c27e119a29c5
Bui Khoi <anhbuikhoi@gmail.com>
2021-12-05 15:40:56 +0700

    update dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
+import plotly.express as px
 from dash.dependencies import Input, Output
 
 
@@ -30,23 +31,6 @@ num_movies = df.shape[0]
 app = dash.Dash(__name__)
 
 server = app.server
-
-# Graph 1
-fig1 = go.Figure()
-
-graph_1_data = df.groupby('release_date', as_index=False)[
-    'gross_worldwide'].sum()
-
-fig1.add_trace(go.Bar(x=graph_1_data['release_date'],
-                      y=graph_1_data['gross_worldwide']))
-
-fig1.update_xaxes(title_text="Year", tickmode='linear')
-
-fig1.update_yaxes(title_text="Gross")
-
-fig1.update_layout(
-    margin=dict(l=20, r=20, t=20, b=20),
-    paper_bgcolor="White",)
 
 # Main layout
 app.layout = html.Div(children=[
@@ -137,21 +121,37 @@ app.layout = html.Div(children=[
         ], style={'display': 'flex', 'flex-direction': 'row', 'margin': '10px 0'}),
         # Row 3
         html.Div(children=[
+            # Graph - 3
             html.Div(children=[
-                html.H3('Top phim'),
-                html.Br(),
-                html.P('Biểu đồ')
-            ], style=CONTAINER_STYLE()),
-            html.Div(children=[
-                html.H3('Gross vs Score'),
-                html.Br(),
-                html.P('Biểu đồ')
-            ], style=CONTAINER_STYLE()),
+                 html.Div(children=[
+                     # Graph 1 Label
+                     html.H3('Top phim',  style={
+                         'flex': 1, 'max-width': '100%'}),
+
+                     # Graph 1 Label
+                     dcc.Dropdown(
+                         id='graph-3-dropdown',
+                         options=[
+                            {'label': 'Ascending', 'value': 'A'},
+                            {'label': 'Decending', 'value': 'D'},
+                         ],
+                         clearable=False,
+                         value='A', style={'flex': 1,
+                                           'appearance': 'none',
+                                           'max-width': '150px',
+                                           'padding-right': '20px'}
+                     ),
+                 ], style={'display': 'flex',
+                           'flex-direction': 'row',
+                           'column-gap': '20px',
+                           'justify-content': 'space-between'}),
+                 dcc.Graph(id='graph-3')
+                 ], style=CONTAINER_STYLE(1)),
+            ###############################
             html.Div(children=[
                 html.H3('Cat vs Gross'),
-                html.Br(),
-                html.P('Biểu đồ')
-            ], style=CONTAINER_STYLE())
+                dcc.Graph(id='graph-4')
+            ], style=CONTAINER_STYLE(1))
         ], style={'display': 'flex', 'flex-direction': 'row', 'margin': '10px 0'})
     ], style={'flex': 6}),
 
@@ -171,10 +171,9 @@ def update_fig1(type):
     else:
         graph_1_data = df.groupby('release_date', as_index=False)[
             'gross_worldwide'].sum()
-    fig1 = go.Figure()
 
-    fig1.add_trace(go.Bar(x=graph_1_data['release_date'],
-                          y=graph_1_data['gross_worldwide']))
+    fig1 = px.area(graph_1_data, x='release_date',
+                   y='gross_worldwide')
 
     fig1.update_xaxes(title_text="Year", tickmode='linear')
 
@@ -200,20 +199,22 @@ def update_fig2(type):
         values = graph_2_data.values
         values = np.append(values, 100-values.sum())
         labels = graph_2_data.index
-        labels = np.append(labels, 'Other')
+        labels = np.append(labels, 'Others')
     elif type == 'L':
         graph_2_data = df.languages.value_counts().head(9)/df.shape[0] * 100
         values = graph_2_data.values
         values = np.append(values, 100-values.sum())
         labels = graph_2_data.index
-        labels = np.append(labels, 'Other')
+        labels = np.append(labels, 'Others')
     elif type == 'C':
         graph_2_data = df.countries_of_origin.value_counts().head(10) / \
             df.shape[0] * 100
         values = graph_2_data.values
         values = np.append(values, 100-values.sum())
         labels = graph_2_data.index
-        labels = np.append(labels, 'Other')
+        labels = np.append(labels, 'Others')
+
+    labels = [label.strip(',').replace(',', ', ') for label in labels]
 
     fig2 = go.Figure()
 
@@ -228,6 +229,106 @@ def update_fig2(type):
         paper_bgcolor="White",)
 
     return fig2
+
+# Graph-3 callback
+
+
+@ app.callback(
+    Output(component_id='graph-3', component_property='figure'),
+    Input(component_id='graph-3-dropdown', component_property='value')
+)
+def update_fig2(type):
+    if type == 'A':
+        graph_3_data = df[['title', 'gross_worldwide']].sort_values(
+            'gross_worldwide', ascending=False).head(10)
+
+        graph_3_data = graph_3_data.sort_values('gross_worldwide')
+    else:
+        graph_3_data = df[['title', 'gross_worldwide']].sort_values(
+            'gross_worldwide', ascending=True).head(10)
+
+        graph_3_data = graph_3_data.sort_values(
+            'gross_worldwide', ascending=False)
+
+    fig3 = go.Figure()
+
+    fig3.add_trace(go.Bar(x=graph_3_data['gross_worldwide'],
+                          y=graph_3_data['title'], orientation='h'))
+    fig3.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="White",
+    )
+    fig3.update_xaxes(title_text="Gross")
+    return fig3
+
+# Graph-4 callback
+
+
+@ app.callback(
+    Output(component_id='graph-4', component_property='figure'),
+    Input(component_id='graph-2-dropdown', component_property='value')
+)
+def update_fig2(type):
+    values = []
+    labels = []
+
+    if type == 'G':
+        graph_2_data = df.genres.value_counts().head(5)/df.shape[0] * 100
+        values = graph_2_data.values
+        values = np.append(values, 100-values.sum())
+        labels = graph_2_data.index
+        labels = np.append(labels, 'Others')
+    elif type == 'L':
+        graph_2_data = df.languages.value_counts().head(9)/df.shape[0] * 100
+        values = graph_2_data.values
+        values = np.append(values, 100-values.sum())
+        labels = graph_2_data.index
+        labels = np.append(labels, 'Others')
+    elif type == 'C':
+        graph_2_data = df.countries_of_origin.value_counts().head(10) / \
+            df.shape[0] * 100
+        values = graph_2_data.values
+        values = np.append(values, 100-values.sum())
+        labels = graph_2_data.index
+        labels = np.append(labels, 'Others')
+
+    labels = [label.strip(',').replace(',', ', ') for label in labels]
+
+    fig4 = go.Figure()
+
+    fig4.add_trace(go.Bar(y=values,
+                   x=labels))
+    fig4.update_traces(marker_color='green')
+    fig4.update_layout(
+        xaxis=dict(
+            showticklabels=False
+        ),
+        margin=dict(l=20, r=20, t=20, b=20),
+        legend=dict(orientation="h",
+                    xanchor="center",
+                    x=0.5),
+
+        paper_bgcolor="White",)
+
+    fig4 = go.Figure()
+
+    # Build dataframe
+
+    for i, l, v in zip(range(0, len(labels)), labels, values):
+        fig4.add_trace(go.Bar(x=[l],
+                              y=[v],
+                              name=l))
+
+    fig4.update_xaxes(showticklabels=False)
+
+    fig4.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        legend=dict(orientation="h",
+                    xanchor="center",
+                    x=0.5),
+        paper_bgcolor="White",)
+
+    return fig4
 
 
 if __name__ == '__main__':
